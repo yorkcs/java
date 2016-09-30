04a7278872f05608111edb721c3117ef15d30b7a
Casey York <yorkcs@outlook.com>
2016-09-29 20:00:19 -0400

    Update ComputeArea.java

--- ComputeArea.java
+++ ComputeArea.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class ComputeArea {
-	public static void main(String[] args) {
-		// creating a scanner object
-		Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        // creating a scanner object
+        Scanner input = new Scanner(System.in);
 
-		// ask user for radius
-		System.out.print("Enter a number for radius: ");
-		double radius = input.nextDouble();
+        // ask user for radius
+        System.out.print("Enter a number for radius: ");
+        double radius = input.nextDouble();
 
-		// compute area
-		double area = radius * radius * 3.14159;
+        // compute area
+        double area = radius * radius * 3.14159;
 
-		// display results
-		System.out.println("The area for the circle of radius " + radius + " is " + area);
-	}
+        // display results
+        System.out.println("The area for the circle of radius " + radius + " is " + area);
+    }
 }
